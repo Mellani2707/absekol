ba6bdbbdf03b2a2cdaa0df0bc7f2912d7f43d091
Mellani2707 <mellaniramadhani2707@gmail.com>
2024-06-04 10:14:03 +0700

    first init github

---
+++ skripsi/test.jsx
@@ -0,0 +1,69 @@
+import React, {Component} from 'react';
+import {Text, View, PermissionsAndroid} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+
+class test extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      la: 0,
+      lo: 0,
+      isMocked: false,
+      range: 0,
+    };
+  }
+
+  componentDidMount() {
+    const requestACCESS_FINE_LOCATIONPermission = async () => {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: 'Izin Mengakses GPS',
+            message: 'Aplikasi ini harus mengakses Lokasi mu',
+            buttonNeutral: 'Nanti Aja',
+            buttonNegative: 'Batal/Cegah',
+            buttonPositive: 'Izinkan',
+          },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('Aplikasi Dapat mengakses lokasimu');
+          Geolocation.getCurrentPosition(
+            position => {
+              console.log(position.mocked);
+              this.setState({
+                isMocked: position.mocked,
+                la: position.coords.latitude,
+                lo: position.coords.longitude,
+              });
+            },
+            error => {
+              console.log(error.code, error.message);
+            },
+            {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+          );
+        } else {
+          console.log(
+            'Akses lokasi ditolak, aplikasi mungkin tidak dapat digunakan dengan baik',
+          );
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    requestACCESS_FINE_LOCATIONPermission();
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Test call geolocation properties</Text>
+        <Text>Koordinat Latitude saat ini: {this.state.la}</Text>
+        <Text>Koordinat Longtitude saat ini: {this.state.lo}</Text>
+        <Text>Is Mocked: {this.state.isMocked ? 'Yes' : 'No'}</Text>
+      </View>
+    );
+  }
+}
+
+export default test;
---
+++ src/component/LoginScreen.jsx
@@ -0,0 +1,100 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  StatusBar,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import LoginButton from './LoginButton';
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Lakukan validasi email dan password jika diperlukan
+
+    // Kirim permintaan login ke backend
+    fetch('http://172.16.4.72:8000/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Tangani respon dari backend
+        if (data.status === 'success') {
+          // Login berhasil
+          console.log('User logged in successfully');
+          console.log('Token:', data.data.token);
+          // Simpan token ke penyimpanan lokal jika diperlukan
+        } else {
+          // Login gagal, tampilkan pesan kesalahan
+          console.error('Login failed:', data.message);
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <Icon name="user-circle" size={100} color="blue" style={styles.icon} />
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        keyboardType="email-address"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+      <LoginButton color="blue" text="Login" onPress={handleLogin} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  icon: {
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '80%',
+    height: 50,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 10,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    backgroundColor: '#ffffff',
+  },
+});
+
+export default LoginScreen;
