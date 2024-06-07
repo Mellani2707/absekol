3c208226e161052db465edd20f8a8db8d0705c53
Rahmat Nur <v-rahmat.nur@dana.id>
2024-06-07 23:13:38 +0700

    swagger

--- absekol-backend/swagger/swaggerConfig.js
+++ absekol-backend/swagger/swaggerConfig.js
@@ -11,6 +11,7 @@ const options = {
         servers: [
             {
                 url: 'http://localhost:3002',
+                url: 'https://absekol-api.numpang.my.id',
             },
         ],
     },
