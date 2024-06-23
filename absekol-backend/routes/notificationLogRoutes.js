8491cb7f57ac0f81e8f173c03681338ccd4f4cbd
Ranur-react <rahmatnur845@gmail.com>
2024-06-23 19:01:59 +0700

    notfilog by uid

--- absekol-backend/routes/notificationLogRoutes.js
+++ absekol-backend/routes/notificationLogRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {
 createNotificationLogController,
+    getNotificationLogByUidController, 
 geteNotificationLogController,
 updateNotificationLogController,
 deleteNotificationLogController
