f49c4399a8f3a37491bdb70b1b1618acdf8f1150
Ranur-react <rahmatnur845@gmail.com>
2024-06-24 14:04:34 +0700

    deploy waweb api

--- waweb-api/respondBuilder.js
+++ waweb-api/respondBuilder.js
@@ -1,13 +1,15 @@
-import axios from 'axios';
-
-const respondBuilderText = async (body, state = {}, userId) => {
-    let responses = {
+const respondBuilderText = (body, state = {}) => {
+    const responses = {
         "info": {
-            text: "Silakan pilih informasi layanan dengan mengetik angka nomor opsi layanan:\n1. Informasi Login\n2. Informasi Lokasi Absensi\n3. Informasi Nomor Whatsapp Guru",
+            text: "Silakan pilih informasi layanan dengan mengetik angka nomor opsi layanan:\n1. Informasi Login\n2. Informasi Lokiasi Absensi\n3. Informasi Nomor Whatsapp Guru",
+            nextState: "infoMenu"
+        },
+        "infoMenu_1": {
+            text: "Akun anda terdaftar pada aplikasi Absekol dengan deatil berikut:\n Username : ${username} \nEmail: {useremail}\nPassword\nRole: {RoleName}",
             nextState: "infoMenu"
         },
         "infoMenu_2": {
-            text: "Pengambilan absen wajib paling jauh di depan gerbang sekolah untuk dapat dianggap sebagai masuk dan berada di area sekolah.",
+            text: "Pengambilan absen  wajib [aling jauh didepan gerbang sekolah untuk dapat dianggap sebagai masuk dan berada di area sekolah",
             nextState: "infoMenu"
         },
         "infoMenu_3": {
@@ -23,64 +25,14 @@ const respondBuilderText = async (body, state = {}, userId) => {
             nextState: state.currentState
         }
     };
-    console.log('=================Decalre Data default===================');
-    console.log(responses);
-    console.log('====================================');
 
     const key = `${state.currentState ? state.currentState + "_" : ""}${body}`.trim() || body;
-    console.log('==============Current Key XX======================');
+    console.log('=================[body]===================');
+    console.log(body);
+    console.log('====================================');
+    console.log('=================[Key]===================');
     console.log(key);
     console.log('====================================');
-    // Dynamic user data fetching
-    if (key === 'infoMenu_1') {
-        try {
-
-
-            let config = {
-                method: 'get',
-                maxBodyLength: Infinity,
-                url: 'https://absekol-api.numpang.my.id/api/users/nowa/',
-                headers: {}
-            };
-
-            axios.request(config)
-                .then((response) => {
-                    console.log(JSON.stringify(response.data));
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-
-            // const userResponse = await axios.get('https://absekol-api.numpang.my.id/api/users/nowa/' + userId); // Replace with your API endpoint
-            // const userdata = userResponse.data;
-            // if (userdata != null) {
-            //     responses["infoMenu_1"] = {
-            //         text: `Akun Anda terdaftar pada aplikasi Absekol dengan detail berikut:\nUsername: ${username}\nEmail: ${useremail}\nPassword: ${password}\nRole: ${RoleName}`,
-            //         nextState: "infoMenu"
-            //     };
-            // } else {
-            //     responses["infoMenu_1"] = {
-            //         text: "Maaf, terjadi kesalahan saat mengambil data pengguna. Silakan coba lagi nanti.",
-            //         nextState: "infoMenu"
-            //     };
-            // }
-
-        } catch (error) {
-            console.error("Error fetching user data:", error);
-            responses["infoMenu_1"] = {
-                text: "Maaf, terjadi kesalahan saat mengambil data pengguna. Silakan coba lagi nanti.",
-                nextState: "infoMenu"
-            };
-        }
-        console.log('================Respind get Menu Info Login====================');
-        console.log(responses["infoMenu_1"]);
-        console.log('====================================');
-    }
-    responses["infoMenu_1"] = {
-        text: "test",
-        nextState: "infoMenu"
-    };
-
     return responses[key] || responses.default;
 };
 
